ddd295d483bee33b57f00dfff97cfc7a19c3d8b1
MUHAMMAD BAHTIAR <muhammad.bahtiar@sigma.co.id>
2024-01-24 19:14:42 +0700

    Update Search

---
+++ assets/js/index.js
@@ -0,0 +1,26 @@
+
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function checkCookie() {
+    var ld = document.getElementById('button-profile');
+    let user = getCookie("token");   
+    if (user == "") {
+        ld.style.display = "none";
+    }else{
+        ld.style.display = "block";
+    }
+}
